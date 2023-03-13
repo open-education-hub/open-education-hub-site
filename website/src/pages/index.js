a93f431959c4201b942383af3c01960778bdf91a
Razvan Deaconescu <razvan.deaconescu@upb.ro>
2023-03-13 07:05:22 +0200

    website: Remove central logo
    Only leave top-left logo, in accordance with communication requirements.
    
    Signed-off-by: Razvan Deaconescu <razvan.deaconescu@upb.ro>

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -13,9 +13,6 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div id="image" style={{display : 'inline-block', paddingRight: '1em'}}>
-          <a href="https://www.eeagrants.ro/"><img src="/img/eea-grants.png" title="EEA Grants" width="80" align="center"/></a>
-        </div>
         <div id="text" style={{display : 'inline-block'}}>
           <p align="center" style={{fontSize: '0.75em'}}>Project <strong>21-COP-0016</strong> funded by <a href="https://www.eeagrants.ro/" style={{color: 'white'}}>EEA Grants</a> (via <a href="https://www.anpcdefp.ro/" style={{color: 'white'}}>ANPCDEFP</a>). <strong>Working together for a green, competitive and inclusive Europe.</strong></p>
         </div>
